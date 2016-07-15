da5efb94a43ecead61f0e5ebd7aea2fbf5dbb167
arpanpathak <arpanpathak1996@gmail.com>
2016-07-15 16:21:38 +0530

    some useless files are being deleted

--- newfile.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-    int main()
-    {
-      printf("Hello PHP-C world");
-      return 0;
-    }
--- test.java
+++
@@ -1,7 +0,0 @@
-public class test
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Java is running successfully on apache....");
-	}
-}
